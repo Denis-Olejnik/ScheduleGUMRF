97a31fa5ff8af18526221cc6600a6e7278996a36
Denis Oleynik <denis1riddle@gmail.com>
2022-02-15 19:47:15 +0300

    Preparation for writing command logic

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ from aiogram.utils import executor
 from config import HOOK_URL
 import psycopg2 as ps
 
-logging.basicConfig(level=logging.INFO, filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, filename='app.log', filemode='w',
+                    format='%(name)s - %(levelname)s - %(message)s')
 
 database = ps.connect(os.environ.get("DATABASE_URL"), sslmode='require')
 curr = database.cursor()
@@ -27,9 +28,24 @@ async def on_shutdown(dp):
     database.close()
 
 
-@dp.message_handler()
+@dp.message_handler(commands=['start'], commands_prefix='!/', ignore_case=True)
 async def on_start_message(message: types.Message):
-    await message.answer("Я получил твоё сообщение!")
+    await message.answer(f"I got ur /start message,{message.from_user.first_name}!")
+
+
+@dp.message_handler(commands=['new'], commands_prefix='!/', ignore_case=True)
+async def on_start_message(message: types.Message):
+    await message.answer(f"I got ur /new message,{message.from_user.first_name}!")
+
+
+@dp.message_handler(commands=['edit'], commands_prefix='!/', ignore_case=True)
+async def on_start_message(message: types.Message):
+    await message.answer(f"I got ur /edit message,{message.from_user.first_name}!")
+
+
+@dp.message_handler(commands=['delete'], commands_prefix='!/', ignore_case=True)
+async def on_start_message(message: types.Message):
+    await message.answer(f"DON'T TOUCH IT! OK?")
 
 
 executor.start_webhook(dispatcher=dp,
--- loader.py
+++
@@ -1,4 +0,0 @@
-import logging
-
-logging.basicConfig(level=logging.INFO, filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
-
