3241e93529c696bef364883de01a27a3ad4dfbab
S8who <s8productive@gmail.com>
2022-02-15 12:31:42 +0300

    Добавлен main.py
    Добавлен основной код для софта

---
+++ main.py
@@ -0,0 +1,30 @@
+import datetime
+import makara
+
+
+weekdays = {1:"Понедельник",2:"Вторник",3:"Среда",4:"Четверг",5:"Пятница",6:"Суббота",7:"Воскресенье"}
+deno_nume = {1:"Числитель",0:"Знаменатель"}
+final_print = {}
+
+reader = datetime.datetime.today().isocalendar()[2]
+week = datetime.datetime.today().isocalendar()[1]
+
+
+print("Сегодня:",weekdays[reader],end=", ")
+
+final = int(week) % 2
+
+print(deno_nume[final],"\n")
+
+c = 1
+
+if int(reader) == 1:
+	handler = makara.numerator[reader-1]
+	for i in range(0,len(makara.numerator[reader])):
+		print(str(c)+")",handler[i])
+		c+=1
+else:
+	handler = makara.denominator[reader-1]
+	for i in range(0,len(makara.denominator[reader])):
+		print(str(c)+")",handler[i])
+		c+=1
