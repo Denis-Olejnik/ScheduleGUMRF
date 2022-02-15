de706d41cb47e645abf8ffe642a69b72f1d1bdbc
Denis Oleynik <denis1riddle@gmail.com>
2022-02-15 18:27:11 +0300

    Update config.py

--- data/config.py
+++ data/config.py
@@ -6,15 +6,16 @@ env = Env()
 env.read_env()
 
 # Database
-DB_HOST = env.str("DB_HOST")
-DB_PORT = env.str("DB_PORT")
-DB_NAME = env.str("DB_NAME")
-DB_USER = env.str("DB_USER")
-DB_PASS = env.str("DB_PASS")
+# DB_HOST = env.str("DB_HOST")
+# DB_PORT = env.str("DB_PORT")
+# DB_NAME = env.str("DB_NAME")
+# DB_USER = env.str("DB_USER")
+# DB_PASS = env.str("DB_PASS")
 
 
 # Heroku
-HOOK_URL = env.str("HOOK_URL")
+# HOOK_URL = env.str("HOOK_URL")
+HOOK_URL = "https://schedule-gumrf.herokuapp.com/"
 
 # Telegram
 # TG_TOKEN = env.str("TG_TOKEN")
