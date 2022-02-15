fa8e1e14da46a5c1a22827d849cddf2daecabde8
S8who <s8productive@gmail.com>
2022-02-15 14:08:52 +0300

    Добавлен вывод в один словарь

--- main.py
+++ main.py
@@ -1,30 +1,32 @@
 import datetime
-import makara
+import MakaraTest
 
 
 weekdays = {1:"Понедельник",2:"Вторник",3:"Среда",4:"Четверг",5:"Пятница",6:"Суббота",7:"Воскресенье"}
-deno_nume = {1:"Числитель",0:"Знаменатель"}
-final_print = {}
+deno_nume = {0:"Числитель",1:"Знаменатель"}
+
+final_dict = {}
+final_list = []
 
 reader = datetime.datetime.today().isocalendar()[2]
 week = datetime.datetime.today().isocalendar()[1]
 
-
-print("Сегодня:",weekdays[reader],end=", ")
-
 final = int(week) % 2
-
-print(deno_nume[final],"\n")
 
 c = 1
 
 if int(reader) == 1:
-	handler = makara.numerator[reader-1]
-	for i in range(0,len(makara.numerator[reader])):
-		print(str(c)+")",handler[i])
-		c+=1
+	handler = MakaraTest.numerator[reader-1]
+	for i in range(0,len(MakaraTest.numerator[reader])):
+		final_list.append(handler[i])
 else:
-	handler = makara.denominator[reader-1]
-	for i in range(0,len(makara.denominator[reader])):
-		print(str(c)+")",handler[i])
-		c+=1
+	handler = MakaraTest.denominator[reader-1]
+	for i in range(0,len(MakaraTest.denominator[reader])):
+		final_list.append(handler[i])
+
+final_dict["Fraction"] = deno_nume[final]
+final_dict["Count_of_week"] = week
+final_dict["Weekday"] = weekdays[reader]
+final_dict["lessons"] = final_list
+
+print(final_dict)
