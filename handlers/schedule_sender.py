d6573b7df1552d0e7e97ad1ee15f4288b345365b
Denis Oleynik <denis1riddle@gmail.com>
2022-03-01 11:52:20 +0300

    time zone fix

--- handlers/schedule_sender.py
+++ handlers/schedule_sender.py
@@ -1,5 +1,6 @@
-from datetime import date
+from datetime import datetime
 
+import pytz
 from loguru import logger
 
 from data.config import DEBUG_MODE
@@ -21,8 +22,7 @@ async def show_schedule(message: types.Message):
             user_data = await postgre.execute_read_query(f"SELECT * FROM users WHERE user_id='{user_id}'")
             user_group = f"{user_data[0][2]}_{user_data[0][3]}".replace('-', '_')
 
-            schedule = await get_schedule.get_user_schedule_today(user_group, date.today().isoweekday())
-
+            schedule = await get_schedule.get_user_schedule_today(user_group, datetime.now(pytz.timezone('Europe/Moscow')).isoweekday())
             # if answer contains any data (first val of dict = True)
             if schedule[0]:
                 await dp.bot.send_message(chat_id=message.from_user.id, text=schedule[1],
