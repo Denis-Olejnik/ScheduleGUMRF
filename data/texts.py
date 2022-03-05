2c21cb24062520449c2f6badbc6708f591658202
Denis Oleynik <denis1riddle@gmail.com>
2022-03-05 19:02:00 +0300

    Minimal user survey is done.

--- data/texts.py
+++ data/texts.py
@@ -3,12 +3,11 @@
 TEXT_ON_START_COMMAND = "Привет\nЯ помогу тебе следить за расписанием в твоей группе"
 
 # Data base:
-TEXT_USER_NOT_FOUND_IN_DB = "Пользователь не найден в базе данных"
+TEXT_USER_NOT_FOUND_IN_DB = "Похоже, что мы тебя не нашли в базе данных\.\nПройди, пожалуйста, коротенький опрос\."
 
 # State machine:
-TEXT_SM_WELCOME_MSG = "Опрос запущен"
 TEXT_SM_USER_GROUP_NAME = "Введите название группы:"
-TEXT_SM_USER_SUBGROUP_CODE = "Введите код подгруппы:"
+TEXT_SM_USER_SUBGROUP_CODE = "Выберите код подгруппы:"
 # TEXT_SM_USER_IS_LEADER = "Предпоследний вопрос: ты староста группы?"
 TEXT_SM_DATA_VALIDATION = "Проверка введённых данных"
 TEXT_SM_WE_GOT_IT = "Отлично\nЯ всё записал"
